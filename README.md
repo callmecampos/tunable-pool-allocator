5cbe33f90d37aff237abff2585ccef53cb6ec319
Felipe Campos <fmrcampos@berkeley.edu>
2020-11-13 17:07:10 -0800

    tests and documentation

--- README.md
+++ README.md
@@ -26,47 +26,52 @@ make test
 
 ### Assumptions:
 
-Conceptual assumptions:
-1. For this particular memory allocator, the smaller block size allocation takes precedent over larger block sizes, which informs many of the design decisions made in building the allocator. (e.g. filling in larger memory blocks, fragment space, etc. with smaller or smallest memory blocks --> this also leads to greatest minimization of fragmentation when possible, with the tradeoff being embedded in the assumption itself, that larger memory allocation has lower precedence/priority).
+Conceptual assumptions: Two assumptions are made concerning usage of smaller sized blocks for this particular memory allocator.
+1. It's assumed that the user prefers smaller block size allocation and therefore allocates memory for such objects more often.
+2. Memory allocations of the same type/size often happen in succession, so we should keep a reference to a pool of said size whenever it is most recently used.
 
-Logistical assumptions:
-1. Heap itself can be used to store state.
+Design decisions and tradeoffs:
+1. Heap itself can and should be used to store state.
+    1. Tradeoff: This ensures simplicity and elegance in the implementation. We do sacrifice some memory and alignment in the process, but it's fairly negligible.
 1. Heap is subdivided evenly by number of pools, giving smaller objects more blocks to allocate into.
+1. If a smaller block pool is full, new allocations for said block size take up blocks in the next non-empty pool of greater block size. This is in line with the user preference for smaller block size allocation, even at the expense of larger block size.
+    1. Tradeoff: The tradeoff for the last two logistical assumptions is embedded in the assumption itself that smaller objects are more important to the user of this memory allocator and that larger memory allocation has lower precedence. Additionally, this increases internal fragmentation, and we could allocate larger blocks in subsequent pools to be used for smaller block allocations, however we chose to keep the implementation clean and straightforward while respecting the pools with larger block sizes by not packing their individual blocks with smaller ones, increasing the time to which they get returned to the pool due to crowding of smaller blocks in those valid larger blocks.
+1. 8-byte alignment (for 64-bit processor): Want to make sure memory accesses are as efficient as possible, so are willing to tradeoff some internal fragmentation (for unaligned allocation sizes) for speed.
+1. pool_free() has undefined behavior when passed a pointer is not currently allocated by pool_alloc() (whether because it wasn't allocated in the first place or it was already freed).
+    1. We have the ability to detect unaligned pointers and invalid free calls, however we chose to keep in line with how classical free functions operate.
+1. pool_init() is only called once per a process.
+
+Valid inputs:
 1. The block sizes array is passed in pre-sorted smallest to largest.
-    1. This assumption was made to accomodate O(log(N)) search for pool headers.
+    1. Tradeoff: This places burden on the user to provide a specifically formatted block size list. However, with this assumption was made to accomodate O(log(N)) search for pool headers without having to sort the block size array during initialization.
 1. No duplicate block sizes are passed into the pool initialization function.
 1. No individual block size exceeds its respective allocated pool size.
 1. The number of block sizes doesn't exceed 248.
     1. Smallest x for which (2^16 - 16\*x) / x - x > 0 is at x = 248.125.
     1. The largest block size count at which a valid set of block sizes can exist is therefore {1, 2, 3, 4, 5, ..., 246, 247, 248} which, 8-byte aligned, becomes {8, 8, 8, 8, 8, ..., 248, 248, 248}).
-1. pool_free() has undefined behavior when passed a pointer is not currently allocated by pool_alloc() (whether because it wasn't allocated in the first place or it was already freed).
-1. pool_init() is only called once per a process.
-
-### Tradeoffs:
-
-1. Storing state in the provided heap: This method ensures simplicity and elegance in the implementation. Tradeoff is that you lose some memory, but not much at all really.
-1. 8-byte alignment (for 64-bit processor): Want to make sure memory accesses are as efficient as possible, so are willing to tradeoff fragmentation for speed.
 
 ### Time and Space Complexity Analysis
 
 ```
-b = number of pools
-N = total number of blocks
+b = number of pools (up to 248)
+N = total number of blocks (up to 8190)
 ```
 
 `pool_init`
-Time: O(b + N)
-Space: O(b + N)
+Time - O(b + N)
+Space - O(b + N)
 
 `pool_alloc`:
-Time: O(log(b))
-Space: O(b)
+Time - Best Case (Cache Hit): O(1), Average Case (Binary Search): O(log(b)), Worst Case (All Pools Full): O(b)
+Space - Best Case (Cache Hit): O(1), Average Case: O(b)
 
 `pool_free`:
-Time: O(1)
-Space: O(1)
+Time - O(1)
+Space - O(1)
 
-#### Further Optimizations
-1. Don't populate all block headers at once during initialization. Do it lazily as memory becomes allocated.
-2. External fragmentation between pools may be used to hold smaller size objects (e.g. leftover space after a 1024-byte pool may be split up into several 32-byte blocks). We could have alternatively compacted all the pools together, combining all the leftover space at the end of the heap, but chose not to do this to simplify pointer arithmetic for indexing into pools and their respective blocks.
+#### Further Future Optimizations
+1. Don't populate all block headers at once during initialization. Do it lazily as memory becomes allocated, would bring down initialization to O(b) time and space.
+1. Lazy free list generation also allows us to dynamically allocate more memory to different memory sizes based on past usage rather than fixed assumptions on user desires.
+1. External fragmentation between pools may be used to hold smaller size objects (e.g. leftover space after a 1024-byte pool may be split up into several 32-byte blocks). Filling leftover external fragmentation exclusively with the smallest memory block size leads to the greatest minimization of external fragmentation while also keeping in line with the assumption that small block size allocations are more desirable.
+1. We could alternatively compact all the pools together, combining all the leftover space at the end of the heap, but chose not to do this to simplify pointer arithmetic for indexing into pools and their respective blocks.
 
--- tests/check_pool_alloc.c
+++ tests/check_pool_alloc.c
@@ -7,6 +7,13 @@
 
 #include "../src/pool_alloc.h"
 
+// ================ INLINE FUNCTIONS ====================
+
+int pool_size_bytes(size_t num_pools)
+{
+    return aligned((HEAP_SIZE_BYTES / num_pools) - sizeof(pool_header_t), 8);
+}
+
 // ================= INITIALIZATION =====================
 
 START_TEST(basic_init)
@@ -39,8 +46,9 @@ END_TEST
 START_TEST(max_init)
 {
     size_t arr[248];
-    for (int i = 0; i < 248; i++) {
-        arr[i] = i+1;
+    for (int i = 0; i < 248; i++)
+    {
+        arr[i] = i + 1;
     }
 
     bool pool = pool_init(arr, 248);
@@ -51,14 +59,33 @@ START_TEST(max_init)
 START_TEST(exceeded_init)
 {
     size_t arr[256];
-    for (int i = 0; i < 256; i++) {
-        arr[i] = i+1;
+    for (int i = 0; i < 256; i++)
+    {
+        arr[i] = i + 1;
     }
 
     bool pool = pool_init(arr, 256);
 
     ck_assert(!pool);
 }
+
+START_TEST(unsorted_init)
+{
+    const size_t arr[] = {sizeof(int), sizeof(uint8_t), sizeof(uint16_t)};
+    bool pool = pool_init(arr, 3);
+
+    ck_assert(!pool);
+}
+END_TEST
+
+START_TEST(bad_size_input_init)
+{
+    const size_t arr[] = {sizeof(int), sizeof(uint8_t), sizeof(uint16_t)};
+    bool pool = pool_init(arr, 50);
+
+    ck_assert(!pool);
+}
+END_TEST
 
 Suite *pool_init_suite(void)
 {
@@ -77,37 +104,34 @@ Suite *pool_init_suite(void)
     tcase_add_test(tc_failed, empty_init);
     tcase_add_test(tc_failed, too_big_init);
     tcase_add_test(tc_failed, exceeded_init);
+    tcase_add_test(tc_failed, unsorted_init);
+    tcase_add_test(tc_failed, bad_size_input_init);
     suite_add_tcase(s, tc_failed);
 
     return s;
 }
-
-START_TEST (unsorted_initialization)
-{
-    const size_t arr[] = {sizeof(int), sizeof(uint8_t), sizeof(uint16_t)};
-    bool pool = pool_init(arr, 3);
-
-    ck_assert(!pool);
-}
-END_TEST
 
 // ================= ALLOCATION & FREEING =====================
 
 START_TEST(alloc_check_relative_block)
 {
     const size_t arr[] = {32, 64, 4096};
-    bool pool = pool_init(arr, 3);
+    size_t size = 3;
+    bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
+    int pool_size = pool_size_bytes(size);
+
     uint8_t *last_ptr;
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < size; i++)
     {
-        last_ptr = (uint8_t *) pool_alloc(arr[i]);
-        ck_assert(last_ptr != NULL);
-        for (int j = 0; j < 3; j++)
+        last_ptr = (uint8_t *)pool_alloc(arr[i]);
+        ck_assert_ptr_nonnull(last_ptr);
+        for (int j = 0; j < pool_size / arr[i] - 1; j++)
         {
-            uint8_t *new_ptr = (uint8_t *) pool_alloc(arr[i]);
+            uint8_t *new_ptr = (uint8_t *)pool_alloc(arr[i]);
+            ck_assert_ptr_nonnull(new_ptr);
             ck_assert_msg(new_ptr - last_ptr == arr[i], "Expecting %zu found %ld", arr[i], new_ptr - last_ptr);
             last_ptr = new_ptr;
         }
@@ -117,15 +141,19 @@ START_TEST(alloc_check_relative_block)
 START_TEST(alloc_check_relative_pool)
 {
     const size_t arr[] = {32, 64, 1024, 4096};
-    bool pool = pool_init(arr, 4);
+    size_t size = 4;
+    bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
-    int pool_size = (65536 / 4) - 16;
+    int pool_size = pool_size_bytes(size);
 
     uint8_t *last_ptr = pool_alloc(arr[0]);
-    for (int i = 1; i < 4; i++) {
+    ck_assert_ptr_nonnull(last_ptr);
+    for (int i = 1; i < size; i++)
+    {
         uint8_t *new_ptr = pool_alloc(arr[i]);
+        ck_assert_ptr_nonnull(new_ptr);
         ck_assert_msg(new_ptr - last_ptr == pool_size, "Expecting %d found %ld", pool_size, new_ptr - last_ptr);
         last_ptr = new_ptr;
     }
@@ -138,26 +166,26 @@ START_TEST(alloc_value)
     ck_assert(pool);
 
     uint8_t *num8a = pool_alloc(sizeof(uint8_t));
-    ck_assert(num8a != NULL);
+    ck_assert_ptr_nonnull(num8a);
     *num8a = 10;
 
     uint8_t *num8b = pool_alloc(sizeof(uint8_t));
-    ck_assert(num8b != NULL);
+    ck_assert_ptr_nonnull(num8b);
     *num8b = 100;
 
     uint16_t *num16 = pool_alloc(sizeof(uint16_t));
-    ck_assert(num16 != NULL);
+    ck_assert_ptr_nonnull(num16);
     *num16 = 1000;
 
     uint32_t *num32 = pool_alloc(sizeof(uint32_t));
-    ck_assert(num32 != NULL);
+    ck_assert_ptr_nonnull(num32);
     *num32 = 10000;
 
     ck_assert(*num8a == 10 && *num8b == 100 && *num16 == 1000 && *num32 == 10000);
 }
 END_TEST
 
-START_TEST(alloc_and_free)
+START_TEST(alloc_and_free_mirror_short)
 {
     const size_t arr[] = {sizeof(int), 1024, 2048};
     bool pool = pool_init(arr, 3);
@@ -165,130 +193,252 @@ START_TEST(alloc_and_free)
     ck_assert(pool);
 
     int *p1 = pool_alloc(sizeof(int));
-    ck_assert(p1 != NULL);
+    ck_assert_ptr_nonnull(p1);
 
     int *p2 = pool_alloc(sizeof(int));
-    ck_assert(p2 != NULL);
+    ck_assert_ptr_nonnull(p2);
 
     pool_free(p1);
     pool_free(p2);
 
     int *p3 = pool_alloc(sizeof(int));
-    ck_assert(p3 != NULL && p3 == p2);
+    ck_assert_ptr_nonnull(p3);
+    ck_assert_ptr_eq(p3, p2);
 
     int *p4 = pool_alloc(sizeof(int));
-    ck_assert(p4 != NULL && p4 == p1);
+    ck_assert_ptr_nonnull(p4);
+    ck_assert_ptr_eq(p4, p1);
 
     pool_free(p3);
     pool_free(p4);
 }
 END_TEST
 
-START_TEST(alloc_fill_unfill_blocks_32)
+START_TEST(alloc_and_free_chain_long)
 {
-    const size_t arr[] = {32};
+    const size_t arr[] = {sizeof(int)};
     bool pool = pool_init(arr, 1);
 
     ck_assert(pool);
+    int pool_size = pool_size_bytes(1);
 
-    for (int i = 0; i < (65536 - 16) / 32; i++) {
-        ck_assert(pool_alloc(32) != NULL);
+    int *p1 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p1);
+
+    int *p2 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p2);
+
+    int *p3 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p3);
+
+    int *p4 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p4);
+
+    int *p5 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p5);
+
+    int *p6 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p6);
+
+    pool_free(p1);
+    pool_free(p4);
+    pool_free(p6);
+    pool_free(p3);
+    pool_free(p2);
+    pool_free(p5);
+
+    int *p7 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p7);
+    ck_assert_ptr_eq(p7, p5);
+
+    int *p8 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p8);
+    ck_assert_ptr_eq(p8, p2);
+
+    int *p9 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p9);
+    ck_assert_ptr_eq(p9, p3);
+
+    int *p10 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p10);
+    ck_assert_ptr_eq(p10, p6);
+
+    int *p11 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p11);
+    ck_assert_ptr_eq(p11, p4);
+
+    int *p12 = pool_alloc(sizeof(int));
+    ck_assert_ptr_nonnull(p12);
+    ck_assert_ptr_eq(p11, p3);
+}
+END_TEST
+
+START_TEST(alloc_fill_unfill_blocks_small)
+{
+    const size_t arr[] = {32};
+    size_t size = 1;
+    bool pool = pool_init(arr, size);
+
+    ck_assert(pool);
+
+    int pool_size = pool_size_bytes(size);
+
+    for (int i = 0; i < pool_size / arr[0] - 1; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(32));
     }
 
     uint8_t *last_ptr = pool_alloc(32);
-    ck_assert(pool_alloc(32) == NULL);
+    ck_assert_ptr_nonnull(last_ptr);
+    
+    ck_assert_ptr_null(pool_alloc(32));
 
     pool_free(last_ptr);
 
-    ck_assert(pool_alloc(32) != NULL);
+    ck_assert_ptr_nonnull(pool_alloc(32));
 }
 
-START_TEST(alloc_fill_unfill_blocks_4096)
+START_TEST(alloc_fill_unfill_blocks_large)
 {
     const size_t arr[] = {4096};
-    bool pool = pool_init(arr, 1);
+    size_t size = 1;
+    bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
-    for (int i = 0; i < (65536 - 16) / 4096; i++) {
-        ck_assert(pool_alloc(4096) != NULL);
+    int pool_size = pool_size_bytes(size);
+
+    for (int i = 0; i < pool_size / arr[0] - 1; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(4096));
     }
 
     uint8_t *last_ptr = pool_alloc(4096);
-    ck_assert(pool_alloc(4096) == NULL);
+    ck_assert_ptr_nonnull(last_ptr);
+
+    ck_assert_ptr_null(pool_alloc(4096));
 
     pool_free(last_ptr);
 
-    ck_assert(pool_alloc(4096) != NULL);
+    ck_assert_ptr_nonnull(pool_alloc(4096));
 }
 
 START_TEST(alloc_overflow_pool)
 {
-    const size_t arr[] = {32, 4096};
-    bool pool = pool_init(arr, 2);
+    const size_t arr[] = {32, 64};
+    size_t size = 2;
+    bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
-    for (int i = 0; i < ((65536 / 2) - 16) / 32 + 1; i++) {
-        ck_assert(pool_alloc(32) != NULL);
+    int pool_size = pool_size_bytes(size);
+
+    for (int i = 0; i < pool_size / arr[0]; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(32));
     }
 
-    for (int i = 0; i < ((65536 / 2) - 16) / 4096; i++) {
-        ck_assert(pool_alloc(32) != NULL);
+    for (int i = 0; i < pool_size / arr[1]; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(32));
     }
 
-    uint8_t *last_ptr = pool_alloc(4096);
-    ck_assert(last_ptr != NULL);
-    ck_assert(pool_alloc(32) == NULL);
-    ck_assert(pool_alloc(4096) == NULL);
+    ck_assert_ptr_null(pool_alloc(32));
+    ck_assert_ptr_null(pool_alloc(64));
 }
 
-START_TEST(alloc_free_overflow_pool_fragment)
+START_TEST(alloc_overflow_pool_mix)
 {
-    const size_t arr[] = {32, 4096};
-    bool pool = pool_init(arr, 2);
+    const size_t arr[] = {32, 64};
+    size_t size = 2;
+    bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
-    for (int i = 0; i < ((65536 / 2) - 16) / 32 + 1; i++) {
-        ck_assert(pool_alloc(32) != NULL);
+    int pool_size = pool_size_bytes(size);
+
+    for (int i = 0; i < pool_size / arr[0]; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(32));
     }
 
-    for (int i = 0; i < ((65536 / 2) - 16) / 4096; i++) {
-        ck_assert(pool_alloc(4096) != NULL);
+    for (int i = 0; i < pool_size / arr[1] - 1; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(64));
     }
 
-    uint8_t *last_ptr = pool_alloc(4096);
-    ck_assert(last_ptr != NULL);
-    ck_assert(pool_alloc(32) == NULL);
+    uint8_t *last_ptr = pool_alloc(64);
+    ck_assert_ptr_nonnull(last_ptr);
+
+    ck_assert_ptr_null(pool_alloc(32));
 
     pool_free(last_ptr);
 
-    ck_assert(pool_alloc(32) != NULL);
+    ck_assert_ptr_nonnull(pool_alloc(32));
 }
 
-START_TEST(alloc_overflow_fragment)
+START_TEST(alloc_overflow_pool_mix_flip)
 {
-    // TODO: implement me with for loops
-    const size_t arr[] = {32, 4096};
-    bool pool = pool_init(arr, 2);
+    const size_t arr[] = {32, 64};
+    size_t size = 2;
+    bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
-    // fill up 4096 all the way, then 32 all the way, then keep going with 32
+    int pool_size = pool_size_bytes(size);
+
+    for (int i = 0; i < pool_size / arr[1]; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(64));
+    }
+
+    for (int i = 0; i < pool_size / arr[0]; i++)
+    {
+        ck_assert_ptr_null(pool_alloc(64));
+    }
+
+    for (int i = 0; i < pool_size / arr[0]; i++)
+    {
+        ck_assert_ptr_nonnull(pool_alloc(32));
+    }
 }
 
-START_TEST(alloc_invalid_size)
+START_TEST(alloc_varied_sizes)
 {
-    // TODO: implement me
     const size_t arr[] = {32, 4096};
-    bool pool = pool_init(arr, 2);
+    size_t size = 2;
+    bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
-    ck_assert(pool_alloc(32) != NULL);
-    ck_assert(pool_alloc(4096) != NULL);
-    ck_assert(pool_alloc(10) == NULL);
+    ck_assert_ptr_nonnull(pool_alloc(32));
+    ck_assert_ptr_nonnull(pool_alloc(4096));
+    ck_assert_ptr_nonnull(pool_alloc(10));
+    ck_assert_ptr_nonnull(pool_alloc(64));
+
+    ck_assert_ptr_null(pool_alloc(8192));
+    ck_assert_ptr_null(pool_alloc(0));
+
+    for (int i = 1; i <= 4096; i++)
+    {
+        uint8_t *ptr = pool_alloc(i);
+        ck_assert_ptr_nonnull(ptr);
+        pool_free(ptr);
+    }
+}
+
+START_TEST(alloc_all_sizes)
+{
+    const size_t arr[] = {pool_size_bytes(1)};
+    size_t size = 1;
+    bool pool = pool_init(arr, size);
+
+    for (int i = 1; i < pool_size_bytes(1) - 1024; i++)
+    {
+        uint8_t *ptr = pool_alloc(i);
+        ck_assert_ptr_nonnull(ptr);
+        pool_free(ptr);
+    }
 }
 
 Suite *pool_alloc_suite(void)
@@ -296,7 +446,7 @@ Suite *pool_alloc_suite(void)
     Suite *s;
     TCase *tc_valid;
     TCase *tc_overflow;
-    TCase *tc_failed;
+    TCase *tc_varying;
 
     s = suite_create("PoolAlloc");
 
@@ -304,23 +454,26 @@ Suite *pool_alloc_suite(void)
     tcase_add_test(tc_valid, alloc_check_relative_block);
     tcase_add_test(tc_valid, alloc_check_relative_pool);
     tcase_add_test(tc_valid, alloc_value);
-    tcase_add_test(tc_valid, alloc_and_free);
+    tcase_add_test(tc_valid, alloc_and_free_mirror_short);
     suite_add_tcase(s, tc_valid);
 
     tc_overflow = tcase_create("Fill and overflow allocation.");
-    tcase_add_test(tc_valid, alloc_fill_unfill_blocks_32);
-    tcase_add_test(tc_valid, alloc_fill_unfill_blocks_4096);
+    tcase_add_test(tc_valid, alloc_fill_unfill_blocks_small);
+    tcase_add_test(tc_valid, alloc_fill_unfill_blocks_large);
     tcase_add_test(tc_valid, alloc_overflow_pool);
-    tcase_add_test(tc_valid, alloc_free_overflow_pool_fragment);
-    tcase_add_test(tc_valid, alloc_overflow_fragment);
+    tcase_add_test(tc_valid, alloc_overflow_pool_mix);
+    tcase_add_test(tc_valid, alloc_overflow_pool_mix_flip);
     suite_add_tcase(s, tc_overflow);
 
-    tc_failed = tcase_create("Failed allocation.");
-    tcase_add_test(tc_valid, alloc_invalid_size);
-    suite_add_tcase(s, tc_failed);
+    tc_varying = tcase_create("Varying size allocation.");
+    tcase_add_test(tc_varying, alloc_varied_sizes);
+    tcase_add_test(tc_varying, alloc_all_sizes);
+    suite_add_tcase(s, tc_varying);
 
     return s;
 }
+
+// =============== RUN TEST SUITES ================
 
 int main(void)
 {
