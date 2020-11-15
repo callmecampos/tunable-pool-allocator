2137dbe327b3a28de9b60867db927a816b8ef5b6
Felipe Campos <fmrcampos@berkeley.edu>
2020-11-14 16:49:11 -0800

    comprehensive testing with looping tests

--- tests/check_pool_alloc.c
+++ tests/check_pool_alloc.c
@@ -1,18 +1,21 @@
-#include <config.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <check.h>
+#include <config.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "../src/pool_alloc.h"
 
-// ================ USEFUL FUNCTIONS ====================
+// ========= VARIABLES & FUNCTION DECLARATIONS ==========
 
-int pool_size_bytes(size_t num_pools)
-{
-    return aligned((HEAP_SIZE_BYTES / num_pools) - sizeof(pool_header_t), 8);
-}
+static uint8_t *fill_pool(size_t n);
+int pool_size_bytes(size_t num_pools);
+
+size_t block_sizes[] = {1, 2, 3, 4, 5, 6, 7, 8,
+                        9, 10, 11, 12, 13, 14, 15, 16,
+                        17, 18, 19, 20, 21, 22, 23, 24, 25,
+                        32, 64, 128, 256, 1024, 2048, 4096, 8192, 16384, 32768};
 
 // ================= INITIALIZATION =====================
 
@@ -23,6 +26,23 @@ START_TEST(basic_init)
 {
     const size_t arr[] = {sizeof(int), 1024, 2048};
     bool pool = pool_init(arr, 3);
+
+    ck_assert(pool);
+}
+END_TEST
+
+/**
+ * Initialization with all valid block sizes.
+ */
+START_TEST(varied_init)
+{
+    size_t arr[_i];
+    for (int i = 0; i < _i; i++)
+    {
+        arr[i] = i + 1;
+    }
+
+    bool pool = pool_init(arr, _i);
 
     ck_assert(pool);
 }
@@ -53,36 +73,21 @@ START_TEST(too_big_init)
 END_TEST
 
 /**
- * Initialization with the most valid number of block sizes.
- */
-START_TEST(max_init)
-{
-    size_t arr[248];
-    for (int i = 0; i < 248; i++)
-    {
-        arr[i] = i + 1;
-    }
-
-    bool pool = pool_init(arr, 248);
-
-    ck_assert(pool);
-}
-
-/**
  * Initialization with an invalid number of block sizes.
  */
 START_TEST(exceeded_init)
 {
-    size_t arr[256];
-    for (int i = 0; i < 256; i++)
+    size_t arr[MAX_NUM_POOLS + 1];
+    for (int i = 0; i < MAX_NUM_POOLS + 1; i++)
     {
         arr[i] = i + 1;
     }
 
-    bool pool = pool_init(arr, 256);
+    bool pool = pool_init(arr, MAX_NUM_POOLS + 1);
 
     ck_assert(!pool);
 }
+END_TEST
 
 /**
  * Initialization with an ill-sorted array of block_sizes.
@@ -108,30 +113,6 @@ START_TEST(bad_size_input_init)
 }
 END_TEST
 
-Suite *pool_init_suite(void)
-{
-    Suite *s;
-    TCase *tc_valid;
-    TCase *tc_failed;
-
-    s = suite_create("PoolInit");
-
-    tc_valid = tcase_create("Valid initialization.");
-    tcase_add_test(tc_valid, basic_init);
-    tcase_add_test(tc_valid, max_init);
-    suite_add_tcase(s, tc_valid);
-
-    tc_failed = tcase_create("Failed initialization.");
-    tcase_add_test(tc_failed, empty_init);
-    tcase_add_test(tc_failed, too_big_init);
-    tcase_add_test(tc_failed, exceeded_init);
-    tcase_add_test(tc_failed, unsorted_init);
-    tcase_add_test(tc_failed, bad_size_input_init);
-    suite_add_tcase(s, tc_failed);
-
-    return s;
-}
-
 // ================= ALLOCATION & FREEING =====================
 
 /**
@@ -139,8 +120,8 @@ Suite *pool_init_suite(void)
  */
 START_TEST(alloc_check_relative_block)
 {
-    const size_t arr[] = {32, 64, 4096};
-    size_t size = 3;
+    const size_t arr[] = {2, 4, 8, 17, 32, 64, 1024, 2048, 3400, 4096};
+    size_t size = 10;
     bool pool = pool_init(arr, size);
 
     ck_assert(pool);
@@ -152,23 +133,24 @@ START_TEST(alloc_check_relative_block)
     {
         last_ptr = (uint8_t *)pool_alloc(arr[i]);
         ck_assert_ptr_nonnull(last_ptr);
-        for (int j = 0; j < pool_size / arr[i] - 1; j++)
+        for (int j = 0; j < pool_size / align(arr[i]) - 1; j++)
         {
             uint8_t *new_ptr = (uint8_t *)pool_alloc(arr[i]);
             ck_assert_ptr_nonnull(new_ptr);
-            ck_assert_msg(new_ptr - last_ptr == arr[i], "Expecting %zu found %ld", arr[i], new_ptr - last_ptr); // TODO: clean this up
+            ck_assert_msg(new_ptr - last_ptr == align(arr[i]), "Expecting %zu found %ld", align(arr[i]), new_ptr - last_ptr);
             last_ptr = new_ptr;
         }
     }
 }
+END_TEST
 
 /**
  * Checking that each of our allocated pools in the heap are equidistant on the heap by global pool size.
  */
 START_TEST(alloc_check_relative_pool)
 {
-    const size_t arr[] = {32, 64, 1024, 4096};
-    size_t size = 4;
+    const size_t arr[] = {2, 4, 8, 17, 32, 64, 1024, 2048, 3400, 8192};
+    size_t size = 7;
     bool pool = pool_init(arr, size);
 
     ck_assert(pool);
@@ -181,10 +163,11 @@ START_TEST(alloc_check_relative_pool)
     {
         uint8_t *new_ptr = pool_alloc(arr[i]);
         ck_assert_ptr_nonnull(new_ptr);
-        ck_assert_msg(new_ptr - last_ptr == pool_size, "Expecting %d found %ld", pool_size, new_ptr - last_ptr); // TODO: clean this up
+        ck_assert_msg(new_ptr - last_ptr == pool_size, "Expecting %d found %ld", pool_size, new_ptr - last_ptr);
         last_ptr = new_ptr;
     }
 }
+END_TEST
 
 /**
  * Checking basic pointer allocation and value assignment to those pointers.
@@ -317,9 +300,9 @@ END_TEST
  * Subsequent allocation attempts (when no other blocks are available) should
  * return NULL. Only after a pool_free() call can memory be allocated again.
  */
-START_TEST(alloc_fill_pool_small_blocks)
+START_TEST(alloc_fill_pool_blocks)
 {
-    const size_t arr[] = {32};
+    const size_t arr[] = {block_sizes[_i]};
     size_t size = 1;
     bool pool = pool_init(arr, size);
 
@@ -327,59 +310,65 @@ START_TEST(alloc_fill_pool_small_blocks)
 
     int pool_size = pool_size_bytes(size);
 
-    // Allocate all available 32-byte blocks except 1
-    for (int i = 0; i < pool_size / arr[0] - 1; i++)
-    {
-        ck_assert_ptr_nonnull(pool_alloc(32));
-    }
-
-    // Allocate the last one
-    uint8_t *last_ptr = pool_alloc(32);
-    ck_assert_ptr_nonnull(last_ptr);
+    // Allocate all available blocks
+    uint8_t *last_alloc = fill_pool(arr[0]);
+    ck_assert_ptr_nonnull(last_alloc);
 
     // Can't allocate new memory there
-    ck_assert_ptr_null(pool_alloc(32));
+    ck_assert_ptr_null(pool_alloc(arr[0]));
 
-    pool_free(last_ptr);
+    pool_free(last_alloc);
 
     // Now we can allocate memory there since the last pointer was freed
-    ck_assert_ptr_nonnull(pool_alloc(32));
+    ck_assert_ptr_nonnull(pool_alloc(arr[0]));
 }
+END_TEST
 
 /**
- * Checking our allocation and freeing algorithm once a pool is fully filled.
+ * Checking our allocation algorithm for various pools.
  * 
- * Subsequent allocation attempts (when no other blocks are available) should
- * return NULL. Only after a pool_free() call can memory be allocated again.
+ * Subsequent allocation attempts (when no other blocks in the designated pool are available)
+ * should allocate memory in the pool above it.
  */
-START_TEST(alloc_fill_pool_large_blocks)
+START_TEST(alloc_pool_check)
 {
-    const size_t arr[] = {16384};
-    size_t size = 1;
+    size_t size = _i;
+    size_t arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = i + 1;
+    }
+
     bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
     int pool_size = pool_size_bytes(size);
 
-    // Allocate all available 16384-byte blocks except 1
-    for (int i = 0; i < pool_size / arr[0] - 1; i++)
+    // Fill up the pool for each block size
+    // (backwards so we don't populate larger blocks with smaller ones)
+    for (int b = size - 1; b >= 0; b--)
     {
-        ck_assert_ptr_nonnull(pool_alloc(16384));
+        // Allocate all available blocks
+        uint8_t *last_alloc = fill_pool(arr[b]);
+        ck_assert_ptr_nonnull(last_alloc);
+
+        // Can't allocate new memory there
+        ck_assert_ptr_null(pool_alloc(arr[b]));
+
+        pool_free(last_alloc);
+
+        // Now we can allocate memory there since the last pointer was freed
+        ck_assert_ptr_nonnull(pool_alloc(arr[b]));
     }
 
-    // Allocate the last one
-    uint8_t *last_ptr = pool_alloc(16384);
-    ck_assert_ptr_nonnull(last_ptr);
-
-    // Can't allocate new memory there
-    ck_assert_ptr_null(pool_alloc(16384));
-
-    pool_free(last_ptr);
-
-    // Now we can allocate memory there since the last pointer was freed
-    ck_assert_ptr_nonnull(pool_alloc(16384));
+    // Unable to allocate new memory for any size after all the pools are filled
+    for (int b = 0; b < size; b++)
+    {
+        ck_assert_ptr_null(pool_alloc(arr[b]));
+    }
 }
+END_TEST
 
 /**
  * Checking our allocation and freeing algorithm once a pool is fully filled,
@@ -388,24 +377,27 @@ START_TEST(alloc_fill_pool_large_blocks)
  * Subsequent allocation attempts (when no other blocks in the designated pool are available)
  * should allocate memory in the pool above it.
  */
-START_TEST(alloc_overflow_pool_small)
+START_TEST(alloc_overflow_pool_check)
 {
-    const size_t arr[] = {32, 64};
-    size_t size = 2;
+    size_t size = _i;
+    size_t arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = i + 1;
+    }
+
     bool pool = pool_init(arr, size);
 
     ck_assert(pool);
 
     int pool_size = pool_size_bytes(size);
 
-    // Fill up the pool for each block size
-    for (int b = 0; b < size; b++)
+    // Fill up all the blocks
+    uint8_t *ptr = NULL;
+    do
     {
-        for (int i = 0; i < pool_size / align(arr[b]); i++)
-        {
-            ck_assert_ptr_nonnull(pool_alloc(arr[0]));
-        }
-    }
+        ptr = pool_alloc(arr[0]);
+    } while (ptr != NULL);
 
     // Unable to allocate new memory for any size after all the pools are filled
     for (int b = 0; b < size; b++)
@@ -413,72 +405,7 @@ START_TEST(alloc_overflow_pool_small)
         ck_assert_ptr_null(pool_alloc(arr[b]));
     }
 }
-
-/**
- * Checking our allocation and freeing algorithm once a pool is fully filled,
- * and there lies more space in larger-block-size pools.
- * 
- * Subsequent allocation attempts (when no other blocks in the designated pool are available)
- * should allocate memory in the pool above it.
- */
-START_TEST(alloc_overflow_pool_large)
-{
-    const size_t arr[] = {8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
-    size_t size = 10;
-    bool pool = pool_init(arr, size);
-
-    ck_assert(pool);
-
-    int pool_size = pool_size_bytes(size);
-
-    // Fill up the pool for each block size
-    for (int b = 0; b < size; b++)
-    {
-        for (int i = 0; i < pool_size / align(arr[b]); i++)
-        {
-            ck_assert_ptr_nonnull(pool_alloc(arr[0]));
-        }
-    }
-
-    // Unable to allocate new memory for any size after all the pools are filled
-    for (int b = 0; b < size; b++)
-    {
-        ck_assert_ptr_null(pool_alloc(arr[b]));
-    }
-}
-
-/**
- * Checking our allocation and freeing algorithm once a pool is fully filled,
- * and there lies more space in larger-block-size pools. This time for unaligned block sizes.
- * 
- * Subsequent allocation attempts (when no other blocks in the designated pool are available)
- * should allocate memory in the pool above it.
- */
-START_TEST(alloc_overflow_pool_unaligned)
-{
-    const size_t arr[] = {3, 7, 13, 69, 86, 420, 1000};
-    size_t size = 7;
-    bool pool = pool_init(arr, size);
-
-    ck_assert(pool);
-
-    int pool_size = pool_size_bytes(size);
-
-    // Fill up the pool for each (unaligned) block size
-    for (int b = 0; b < size; b++)
-    {
-        for (int i = 0; i < pool_size / align(arr[b]); i++)
-        {
-            ck_assert_ptr_nonnull(pool_alloc(arr[0]));
-        }
-    }
-
-    // Unable to allocate new memory for any size after all the pools are filled
-    for (int b = 0; b < size; b++)
-    {
-        ck_assert_ptr_null(pool_alloc(arr[b]));
-    }
-}
+END_TEST
 
 /**
  * Checking our allocation and freeing algorithm once a pool is fully filled,
@@ -489,7 +416,7 @@ START_TEST(alloc_overflow_pool_unaligned)
  */
 START_TEST(alloc_overflow_pool_mix)
 {
-    const size_t arr[] = {32, 64};
+    const size_t arr[] = {1024, 4096};
     size_t size = 2;
     bool pool = pool_init(arr, size);
 
@@ -497,32 +424,33 @@ START_TEST(alloc_overflow_pool_mix)
 
     int pool_size = pool_size_bytes(size);
 
-    // Fill up the 32-byte blocks
-    for (int i = 0; i < pool_size / arr[0]; i++)
+    // Fill up the 1024-byte blocks
+    for (int i = 0; i < pool_size / align(arr[0]); i++)
     {
-        ck_assert_ptr_nonnull(pool_alloc(32));
+        ck_assert_ptr_nonnull(pool_alloc(arr[0]));
     }
 
-    // Fill up the 64-byte blocks except for 1
-    for (int i = 0; i < pool_size / arr[1] - 1; i++)
+    // Fill up the 4096-byte blocks except for 1
+    for (int i = 0; i < pool_size / align(arr[1]) - 1; i++)
     {
-        ck_assert_ptr_nonnull(pool_alloc(64));
+        ck_assert_ptr_nonnull(pool_alloc(arr[1]));
     }
 
-    // Allocate the last one
-    uint8_t *last_ptr = pool_alloc(64);
+    // Allocate the last 4096-byte block
+    uint8_t *last_ptr = pool_alloc(arr[1]);
     ck_assert_ptr_nonnull(last_ptr);
 
     // Can't allocate smaller memory anywhere
-    // Since the 32-byte and 64-byte pools are filled
-    ck_assert_ptr_null(pool_alloc(32));
+    // Since the 1024-byte and 4096-byte pools are filled
+    ck_assert_ptr_null(pool_alloc(arr[0]));
 
-    // Release a block on the 64-byte pool
+    // Release a block on the 4096-byte pool
     pool_free(last_ptr);
 
-    // Allocate 32-bytes on the 64-byte pool
-    ck_assert_ptr_nonnull(pool_alloc(32));
+    // Allocate 1024-bytes on the 4096-byte pool
+    ck_assert_ptr_nonnull(pool_alloc(arr[0]));
 }
+END_TEST
 
 /**
  * Checking our allocation and freeing algorithm once a pool is fully filled,
@@ -531,7 +459,7 @@ START_TEST(alloc_overflow_pool_mix)
  */
 START_TEST(alloc_backwards_overflow)
 {
-    const size_t arr[] = {32, 64};
+    const size_t arr[] = {1024, 4096};
     size_t size = 2;
     bool pool = pool_init(arr, size);
 
@@ -539,32 +467,35 @@ START_TEST(alloc_backwards_overflow)
 
     int pool_size = pool_size_bytes(size);
 
-    // Fill up the 64-byte blocks
-    for (int i = 0; i < pool_size / arr[1]; i++)
+    // Fill up the 4096-byte blocks
+    for (int i = 0; i < pool_size / align(arr[1]); i++)
     {
-        ck_assert_ptr_nonnull(pool_alloc(64));
+        ck_assert_ptr_nonnull(pool_alloc(arr[1]));
     }
 
-    // Unsuccessfully allocate more 64-byte memory (it can't be allocated to the available 32-byte blocks)
-    for (int i = 0; i < pool_size / arr[0]; i++)
+    // Unsuccessfully allocate more 4096-byte memory
+    // (since it can't be allocated to the available 1024-byte blocks)
+    for (int i = 0; i < pool_size / align(arr[0]); i++)
     {
-        ck_assert_ptr_null(pool_alloc(64));
+        ck_assert_ptr_null(pool_alloc(arr[1]));
     }
 
-    // Successfully allocate the rest of the 32-byte blocks
-    for (int i = 0; i < pool_size / arr[0]; i++)
+    // Successfully allocate the rest of the 1024-byte blocks
+    for (int i = 0; i < pool_size / align(arr[0]); i++)
     {
-        ck_assert_ptr_nonnull(pool_alloc(32));
+        ck_assert_ptr_nonnull(pool_alloc(arr[0]));
     }
 
     // No more memory to allocate
-    ck_assert_ptr_null(pool_alloc(32));
+    ck_assert_ptr_null(pool_alloc(arr[0]));
+    ck_assert_ptr_null(pool_alloc(arr[1]));
 }
+END_TEST
 
 /**
  * Checking our allocation and freeing algorithm for variable size allocations.
  * Also making sure behavior of size 4096 size blocks for a 2 pool allocation
- * is consistent i.e. you can fit 7 4096 size blocks in (2^16 / 2 - 16) bytes.
+ * is consistent i.e. you can fit 7 full 4096 size blocks in (2^16 / 2 - 16) bytes.
  * 
  * Anything more won't be allowed to allocate, however blocks size 32 and below
  * will fit nicely in the 32-byte block pool.
@@ -614,6 +545,7 @@ START_TEST(alloc_varied_sizes)
     // 32-byte blocks are still available
     ck_assert_ptr_nonnull(pool_alloc(16));
 }
+END_TEST
 
 /**
  * Checking our allocation and freeing algorithm for a single pool with a single block paradigm.
@@ -626,12 +558,41 @@ START_TEST(alloc_all_sizes)
     size_t size = 1;
     bool pool = pool_init(arr, size);
 
+    ck_assert(pool);
+
     for (int i = 1; i < pool_size_bytes(1); i++)
     {
         uint8_t *ptr = pool_alloc(i);
-        ck_assert_ptr_nonnull(ptr);
+        ck_assert_msg(ptr != NULL, "for size %d", i);
         pool_free(ptr);
     }
+}
+END_TEST
+
+// ================ TESTING SUITE DEFINITIONS ==================
+
+Suite *pool_init_suite(void)
+{
+    Suite *s;
+    TCase *tc_valid;
+    TCase *tc_failed;
+
+    s = suite_create("PoolInit");
+
+    tc_valid = tcase_create("Valid initialization.");
+    tcase_add_test(tc_valid, basic_init);
+    tcase_add_loop_test(tc_valid, varied_init, 1, MAX_NUM_POOLS + 1); // exclusive
+    suite_add_tcase(s, tc_valid);
+
+    tc_failed = tcase_create("Failed initialization.");
+    tcase_add_test(tc_failed, empty_init);
+    tcase_add_test(tc_failed, too_big_init);
+    tcase_add_test(tc_failed, exceeded_init);
+    tcase_add_test(tc_failed, unsorted_init);
+    tcase_add_test(tc_failed, bad_size_input_init);
+    suite_add_tcase(s, tc_failed);
+
+    return s;
 }
 
 Suite *pool_alloc_suite(void)
@@ -651,11 +612,9 @@ Suite *pool_alloc_suite(void)
     suite_add_tcase(s, tc_valid);
 
     tc_overflow = tcase_create("Fill and overflow allocation.");
-    tcase_add_test(tc_valid, alloc_fill_pool_small_blocks);
-    tcase_add_test(tc_valid, alloc_fill_pool_large_blocks);
-    tcase_add_test(tc_valid, alloc_overflow_pool_small);
-    tcase_add_test(tc_valid, alloc_overflow_pool_large);
-    tcase_add_test(tc_valid, alloc_overflow_pool_unaligned);
+    tcase_add_loop_test(tc_valid, alloc_fill_pool_blocks, 0, 34);
+    tcase_add_loop_test(tc_valid, alloc_pool_check, 1, MAX_NUM_POOLS + 1);
+    tcase_add_loop_test(tc_valid, alloc_overflow_pool_check, 1, MAX_NUM_POOLS + 1);
     tcase_add_test(tc_valid, alloc_overflow_pool_mix);
     tcase_add_test(tc_valid, alloc_backwards_overflow);
     suite_add_tcase(s, tc_overflow);
@@ -666,6 +625,29 @@ Suite *pool_alloc_suite(void)
     suite_add_tcase(s, tc_varying);
 
     return s;
+}
+
+// ============= HELPER FUNCTIONS =================
+
+static uint8_t *fill_pool(size_t n)
+{
+    uint8_t *last_ptr = NULL;
+    while (true)
+    {
+        uint8_t *ptr = pool_alloc(n);
+        if (ptr == NULL)
+        {
+            break;
+        }
+        last_ptr = ptr;
+    }
+
+    return last_ptr;
+}
+
+int pool_size_bytes(size_t num_pools)
+{
+    return aligned((HEAP_SIZE_BYTES / num_pools) - sizeof(pool_header_t), 8);
 }
 
 // =============== RUN TEST SUITES ================
